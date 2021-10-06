2fd767eb46f56a6ec9d9404e0731038ba4d0852d
developer-suhag <developersuhag@gmail.com>
2021-10-06 16:18:04 +0600

    github

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signOut,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
 import "./App.css";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 
 initializeAuthentication();
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const auth = getAuth();
 
 function App() {
+  const [user, setUser] = useState({});
   const handleGoogleSignIn = () => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider).then((result) => {
-      console.log(result);
+    signInWithPopup(auth, googleProvider).then((result) => {
+      const { displayName, email, photoURL } = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        img: photoURL,
+      };
+      setUser(loggedInUser);
+    });
+  };
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider).then((result) => {
+      const { displayName, email, photoURL } = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        img: photoURL,
+      };
+      setUser(loggedInUser);
+    });
+  };
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
     });
   };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+      {!user.name ? (
+        <div>
+          <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+          <button onClick={handleGithubSignIn}>Sign in with Github</button>
+        </div>
+      ) : (
+        <button onClick={handleSignOut}>Sign Out</button>
+      )}
+      <br />
+      <br />
+      {user.name && (
+        <div>
+          <img style={{ borderRadius: "50%" }} src={user.img} alt="" />
+          <h3>Welcome {user.name}</h3>
+          <p>I know your email address: {user.email}</p>
+        </div>
+      )}
     </div>
   );
 }
